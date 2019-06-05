2571fcd83770a0481ca0661c95b638778c295e79
Trevor Markel <tmarkel6849@gmail.com>
2019-06-05 11:46:19 -0600

    created seed file, getAll query function

--- SDCpostgreSQL/index.js
+++ SDCpostgreSQL/index.js
@@ -3,7 +3,22 @@ const { Pool } = require('pg');
 const pool = new Pool ({
   user: 'sdc',
   host: 'localhost',
-  database: 'hostProfile',
+  database: 'hostprofiles',
   password: 'sdc',
   port: 5432,
 })
+
+
+const getAll = () => {
+  const queryString = 'SELECT * FROM hosts';
+  pool.query(queryString, (err, result) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log(result);
+  })
+}
+
+getAll();
+
+module.exports.pool = pool;
