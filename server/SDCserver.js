ee631e215fffab6e4c3480bb1aaa51f89e5a5cab
Trevor Markel <tmarkel6849@gmail.com>
2019-06-06 20:50:15 -0600

    updated server

--- server/SDCserver.js
+++ server/SDCserver.js
@@ -2,18 +2,26 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
+const { getOne } = require('../SDCpostgreSQL/index.js');
 port = 3005;
 
 const app = express();
 
 app.use(cors());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname + '/../public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
 
-app.get('/', (req, res) => {
-  res.send(200);
+app.get('/postgres', (req, res) => {
+  getOne((data) => {
+    if (!data) {
+      res.send(404);
+      return;
+    }
+    res.send(data[0]);
+  })
 })
 
 app.listen(port, () => {
