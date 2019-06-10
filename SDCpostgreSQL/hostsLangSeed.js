9e4fc18aed2baa7253afbfc848f216e2fbe66d3e
Trevor Markel <tmarkel6849@gmail.com>
2019-06-10 10:51:47 -0600

    updated hostsLanguage seeding script, generating csv correctly

--- SDCpostgreSQL/hostsLangSeed.js
+++ SDCpostgreSQL/hostsLangSeed.js
@@ -1,15 +1,50 @@
-// i want to random generate entries for my join table between hosts and languages
-// create a random number, either 1 or 2, generate entries based on this number
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
-const languageIdx = (num) => {
-  return Math.ceil(Math.random() * num);
+/***********************FUNCTIONS TO CREATE RANDOM ENTRIES*************************/
+
+const totalLanguages = 8;
+
+const languageIdx = () => {
+  return Math.ceil(Math.random() * totalLanguages)
 }
 
-const randomLanguages = () => {
-  const numOfLanguages = Math.ceil(Math.random() * 2);
-  if ( numOfLanguages === 1 ) {
+const hostSpokenLanguages = () => {
+  return Math.ceil(Math.random() * 2)
+}
 
-  } else if ( numOfLanguages  === 2 ) {
+const hostLanguageEntry = (host_id) => {
+  let language_id = languageIdx();
+  return { host_id, language_id }
+}
 
+/**********************WRITE ENTRIES TO CSV*************************/
+
+const csvWriter = createCsvWriter({
+  path: 'hostsLang1.csv',
+  header: [
+    { id: 'host_id', title: 'host_id' },
+    { id: 'language_id', title: 'language_id' }
+  ]
+})
+
+const entries = []
+
+const createCsv = (amount, hostId) => {
+  let spokenLanguages;
+  for ( let i = 0; i < amount; i++ ) {
+    spokenLanguages = hostSpokenLanguages()
+    if ( spokenLanguages === 2 ) {
+      entries.push(hostLanguageEntry(hostId))
+      entries.push(hostLanguageEntry(hostId))
+    } else {
+      entries.push(hostLanguageEntry(hostId))
+    }
+    hostId++
   }
+  csvWriter.writeRecords(entries)
+    .then(() => {
+      console.log('file created...')
+    })
 }
+
+createCsv(10, 1);
