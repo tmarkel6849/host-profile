3e4dbedd1c8b2a365effaf116d9571770abd0499
Trevor Markel <tmarkel6849@gmail.com>
2019-06-17 12:04:56 -0600

    updated server webpack config

--- webpack.server.js
+++ webpack.server.js
@@ -1,32 +1,47 @@
 const path = require('path'),
-      nodeExternals = require('webpack-node-externals'),
-      DIST_DIR = path.join(__dirname, '/public')
+      webpack = require('webpack'),
+      nodeExternals = require('webpack-node-externals')
 
 module.exports = {
-  entry: './server/SDCserver.js',
-
+  mode: 'development',
   target: 'node',
-
   externals: [nodeExternals()],
-
+  entry: ['./server/SDCserver.js'],
   output: {
-    path: DIST_DIR,
-    filename: 'bundleS.js'
+    path: path.resolve(__dirname + 'server'),
+    filename: 'serverSSR.js',
+    publicPath: '/'
   },
-
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: '/node_modules/',
-      use: 'babel-loader'
-    }, {
-      test: /\.jsx?$/,
-      exclude: '/node_modules/',
-      loader: 'babel-loader',
-    }, {
-      test: /\.s?css$/,
-      exclude: '/node_modules/',
-      use: ['style-loader', 'css-loader', 'sass-loader']
-    }]
+    rules: [
+      {
+        test: /.js$/,
+        loader: 'babel-loader',
+        include: path.resolve(__dirname, 'client'),
+        exclude: '/node_modules/',
+        options: {
+          presets: [['env', { modules: false }, 'react']],
+          plugins: [
+            ['transform-object-rest-spread', { useBuildIns: true }],
+            'transform-class-properties'
+          ]
+        }
+      },
+      {
+        test: /\.scss$/,
+        loader: 'ignore-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: 'ignore-loader'
+      },
+      {
+        test: /\.(jpg|png|svg|gif|pdf)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]'
+        }
+      }
+    ]
   }
-};
+}
